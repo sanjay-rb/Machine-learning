dbd64df9fff22872c1c817a8694dfcfb849febfc
sanjaysanju618 <sanjaybabu618@gmail.com>
2021-09-03 14:26:40 +0530

    Removed Audio bloging

--- notebook/audioplayer.py
+++
@@ -1,7 +0,0 @@
-from IPython.display import Audio
-
-def play(filename):
-    try:
-        return Audio(f"../recording/{filename}", autoplay=False)
-    except ValueError:
-        return "Please check the presence of audio file under recording folder (or) audio file name and extenstion"
